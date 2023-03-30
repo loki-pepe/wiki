35a3161e4224aa7f851f769bd31cf07039616cc2
loki-pepe <lovro-p@DESKTOP-6TT96MA>
2023-03-30 17:35:37 +0200

    Add entry.html, error.html, search.html and update index.html, layout.html, urls.py, views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
+
+markdowner = Markdown()
 
 
 def index(request):
@@ -8,3 +12,29 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, entry):
+    try:
+        index = list(map(str.lower, util.list_entries())).index(entry.lower())
+    except ValueError:
+        return render(request, "encyclopedia/error.html")
+
+    entry = util.list_entries()[index]
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry,
+        "entry": markdowner.convert(util.get_entry(entry))
+    })
+
+def search(request):
+    try:
+        index = list(map(str.lower, util.list_entries())).index(request.GET["q"].lower())
+    except ValueError:
+        entries = []
+        for entry in util.list_entries():
+            if request.GET["q"].lower() in entry.lower():
+                entries.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "entries": entries,
+            "query": request.GET["q"]
+        })
+
+    return HttpResponseRedirect(f"wiki/{util.list_entries()[index]}")
